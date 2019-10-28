44e93449aa9e818ede2f7e50665024785ebc1449
mragicl <mragicl@protonmail.com>
2019-10-28 18:48:13 +0100

    Add requirements.txt for pip

--- install_idex_monitor.sh
+++ install_idex_monitor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-docker create grafana-storage
-docker create influxdb-storage
+
+#uncomment in case virtualenv is not installed
+#sudo apt install python-virtualenv
 docker pull mragicl/docker-idex-monitor
+docker volume create grafana-storage
+docker volume create influxdb-storage
 docker run -d   --name docker-idex-monitor   -p 3003:3003   -p 3004:8888   -p 8086:8086   -v influxdb-storage:/var/lib/influxdb   -v grafana-storage:/var/lib/grafana   mragicl/docker-idex-monitor
